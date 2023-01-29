c8339be5ca655f5c28528ea0832def2243c1fea2
Erik Olofsson <erik@olofsson.info>
2023-01-29 01:22:42 +0100

    [clang] Add support for instrumenting coroutine functions based on return type

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -8798,6 +8798,20 @@ static void HandleHLSLParamModifierAttr(QualType &CurType,
     CurType = S.getASTContext().getLValueReferenceType(CurType);
 }
 
+static void HandleInstrumentNonCoroutineFunctionEnterDisable(
+  TypeProcessingState &State,
+  QualType &CurType,
+  ParsedAttr &Attr) {
+  if (State.getDeclarator().isDeclarationOfFunction()) {
+    CurType = State.getAttributedType(
+        createSimpleAttr<InstrumentNonCoroutineFunctionEnterDisableAttr
+          >(State.getSema().Context, Attr),
+          CurType, CurType);
+  } else {
+    Attr.diagnoseAppertainsTo(State.getSema(), (Decl const *)nullptr);
+  }
+}
+
 static void processTypeAttrs(TypeProcessingState &state, QualType &type,
                              TypeAttrLocation TAL,
                              const ParsedAttributesView &attrs,
@@ -8943,6 +8957,12 @@ static void processTypeAttrs(TypeProcessingState &state, QualType &type,
     case ParsedAttr::AT_LifetimeBound:
       if (TAL == TAL_DeclChunk)
         HandleLifetimeBoundAttr(state, type, attr);
+      break;
+    case ParsedAttr::AT_InstrumentNonCoroutineFunctionEnterDisable:
+      if (TAL == TAL_DeclChunk) {
+        HandleInstrumentNonCoroutineFunctionEnterDisable(state, type, attr);
+      	attr.setUsedAsTypeAttr();
+      }
       break;
 
     case ParsedAttr::AT_NoDeref: {
