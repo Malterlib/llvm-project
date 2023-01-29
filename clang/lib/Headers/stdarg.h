b19e0b52bfac0546c62d1aff8e21a55e4e805dd7
Erik Olofsson <erik@olofsson.info>
2023-01-29 01:14:34 +0100

    [clang] Define _VA_LIST_DEFINED in stdarg.h

--- clang/lib/Headers/stdarg.h
+++ clang/lib/Headers/stdarg.h
@@ -14,6 +14,11 @@
 typedef __builtin_va_list va_list;
 #define _VA_LIST
 #endif
+
+#if !defined(_VA_LIST_DEFINED)
+#define _VA_LIST_DEFINED 1
+#endif
+
 #define va_start(ap, param) __builtin_va_start(ap, param)
 #define va_end(ap)          __builtin_va_end(ap)
 #define va_arg(ap, type)    __builtin_va_arg(ap, type)
