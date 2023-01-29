1aced022b135d7728e7042ce47573c2a3676e995
Erik Olofsson <erik@olofsson.info>
2023-01-29 21:48:49 +0100

    Fix UBSan with statically linked libc++abi

--- compiler-rt/lib/ubsan/ubsan_type_hash_itanium.cpp
+++ compiler-rt/lib/ubsan/ubsan_type_hash_itanium.cpp
@@ -85,6 +85,25 @@ public:
 
 namespace abi = __cxxabiv1;
 
+extern "C" {
+SANITIZER_INTERFACE_ATTRIBUTE SANITIZER_WEAK_ATTRIBUTE
+const abi::__si_class_type_info *
+__overidable_ubsan_get_dyncast_si(const abi::__class_type_info *Ptr) {
+  return dynamic_cast<const abi::__si_class_type_info *>(Ptr);
+}
+
+SANITIZER_INTERFACE_ATTRIBUTE SANITIZER_WEAK_ATTRIBUTE
+const abi::__vmi_class_type_info *
+__overidable_ubsan_get_dyncast_wmi(const abi::__class_type_info *Ptr) {
+  return dynamic_cast<const abi::__vmi_class_type_info *>(Ptr);
+}
+
+SANITIZER_INTERFACE_ATTRIBUTE SANITIZER_WEAK_ATTRIBUTE abi::__class_type_info *
+__overidable_ubsan_get_dyncast_class(std::type_info *Ptr) {
+  return dynamic_cast<abi::__class_type_info *>(Ptr);
+}
+}
+
 using namespace __sanitizer;
 
 // We implement a simple two-level cache for type-checking results. For each
@@ -134,11 +153,11 @@ static bool isDerivedFromAtOffset(const abi::__class_type_info *Derived,
     return Offset == 0;
 
   if (const abi::__si_class_type_info *SI =
-        dynamic_cast<const abi::__si_class_type_info*>(Derived))
+        __overidable_ubsan_get_dyncast_si(Derived))
     return isDerivedFromAtOffset(SI->__base_type, Base, Offset);
 
   const abi::__vmi_class_type_info *VTI =
-    dynamic_cast<const abi::__vmi_class_type_info*>(Derived);
+    __overidable_ubsan_get_dyncast_wmi(Derived);
   if (!VTI)
     // No base class subobjects.
     return false;
@@ -171,11 +190,11 @@ static const abi::__class_type_info *findBaseAtOffset(
     return Derived;
 
   if (const abi::__si_class_type_info *SI =
-        dynamic_cast<const abi::__si_class_type_info*>(Derived))
+        __overidable_ubsan_get_dyncast_si(Derived))
     return findBaseAtOffset(SI->__base_type, Offset);
 
   const abi::__vmi_class_type_info *VTI =
-    dynamic_cast<const abi::__vmi_class_type_info*>(Derived);
+    __overidable_ubsan_get_dyncast_wmi(Derived);
   if (!VTI)
     // No base class subobjects.
     return nullptr;
@@ -242,7 +261,7 @@ bool __ubsan::checkDynamicType(void *Object, void *Type, HashValue Hash) {
 
   // Check that this is actually a type_info object for a class type.
   abi::__class_type_info *Derived =
-    dynamic_cast<abi::__class_type_info*>(Vtable->TypeInfo);
+    __overidable_ubsan_get_dyncast_class(Vtable->TypeInfo);
   if (!Derived)
     return false;
 
