d075df813e41cb95ac82555d8438035bd085ea4c
Erik Olofsson <erik@olofsson.info>
2023-06-10 14:26:21 +0200

    [runtimes] Add support for dynamically checking for forked child process
    This fixes crashes in code that runs before the fork handler in TSan runtime

--- compiler-rt/lib/tsan/rtl/tsan_mman.cpp
+++ compiler-rt/lib/tsan/rtl/tsan_mman.cpp
@@ -212,19 +212,19 @@ void *user_alloc_internal(ThreadState *thr, uptr pc, uptr sz, uptr align,
     GET_STACK_TRACE_FATAL(thr, pc);
     ReportOutOfMemory(sz, &stack);
   }
-  if (ctx && ctx->initialized && !ctx->after_multithreaded_fork)
+  if (ctx && ctx->initialized && !ctx->afterMultithreadedFork())
     OnUserAlloc(thr, pc, (uptr)p, sz, true);
-  if (signal && ctx && !ctx->after_multithreaded_fork)
+  if (signal && ctx && !ctx->afterMultithreadedFork())
     SignalUnsafeCall(thr, pc);
   return p;
 }
 
 void user_free(ThreadState *thr, uptr pc, void *p, bool signal) {
   ScopedGlobalProcessor sgp;
-  if (ctx && ctx->initialized && !ctx->after_multithreaded_fork)
+  if (ctx && ctx->initialized && !ctx->afterMultithreadedFork())
     OnUserFree(thr, pc, (uptr)p, true);
   allocator()->Deallocate(&thr->proc()->alloc_cache, p);
-  if (signal && ctx && !ctx->after_multithreaded_fork)
+  if (signal && ctx && !ctx->afterMultithreadedFork())
     SignalUnsafeCall(thr, pc);
 }
 
