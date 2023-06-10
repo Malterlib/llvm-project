6cfcfe46192f71269f8434a4d91c9abdfbd3e265
Erik Olofsson <erik@olofsson.info>
2023-06-10 14:26:21 +0200

    [runtimes] Add support for dynamically checking for forked child process
    This fixes crashes in code that runs before the fork handler in TSan runtime

--- compiler-rt/lib/tsan/rtl/tsan_rtl.cpp
+++ compiler-rt/lib/tsan/rtl/tsan_rtl.cpp
@@ -40,6 +40,10 @@ SANITIZER_WEAK_DEFAULT_IMPL
 void __tsan_test_only_on_fork() {}
 #endif
 
+#if !SANITIZER_GO
+void __tsan_check_forked_parent_or_child();
+#endif
+
 namespace __tsan {
 
 #if !SANITIZER_GO
@@ -816,6 +820,13 @@ int Finalize(ThreadState *thr) {
 }
 
 #if !SANITIZER_GO
+
+bool Context::afterMultithreadedFork() {
+  ScopedIgnoreInterceptors ignore;
+  __tsan_check_forked_parent_or_child();
+  return after_multithreaded_fork;
+}
+
 void ForkBefore(ThreadState* thr, uptr pc) SANITIZER_NO_THREAD_SAFETY_ANALYSIS {
   GlobalProcessorLock();
   SlotLock(thr);
@@ -973,7 +984,7 @@ void TraceSwitchPart(ThreadState* thr) {
   if (TraceSkipGap(thr))
     return;
 #if !SANITIZER_GO
-  if (ctx->after_multithreaded_fork) {
+  if (ctx->afterMultithreadedFork()) {
     // We just need to survive till exec.
     TracePart* part = thr->tctx->trace.parts.Back();
     if (part) {
@@ -1068,7 +1079,7 @@ void ThreadIgnoreBegin(ThreadState* thr, uptr pc) {
   CHECK_GT(thr->ignore_reads_and_writes, 0);
   thr->fast_state.SetIgnoreBit();
 #if !SANITIZER_GO
-  if (pc && !ctx->after_multithreaded_fork)
+  if (pc && !ctx->afterMultithreadedFork())
     thr->mop_ignore_set.Add(CurrentStackId(thr, pc));
 #endif
 }
@@ -1098,7 +1109,7 @@ void ThreadIgnoreSyncBegin(ThreadState *thr, uptr pc) {
   thr->ignore_sync++;
   CHECK_GT(thr->ignore_sync, 0);
 #if !SANITIZER_GO
-  if (pc && !ctx->after_multithreaded_fork)
+  if (pc && !ctx->afterMultithreadedFork())
     thr->sync_ignore_set.Add(CurrentStackId(thr, pc));
 #endif
 }
